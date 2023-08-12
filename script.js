65754abaf79b693bee6695e630695c771cec1acc
Stephen Horvath <s.horvath@outlook.com.au>
2023-08-12 22:40:49 +1000

    Add basic editing

--- script.js
+++ script.js
@@ -63,6 +63,86 @@ function elem_size_resize(e) {
 
 days.addEventListener('input', elem_size_resize);
 
+
+function edit_time(start_e, curr_e) {
+    let time_slot = start_e.target;
+
+    slot_size = time_slot.getBoundingClientRect();
+    const start_pos = { x: start_e.clientX - slot_size.left, y: start_e.clientY - slot_size.top}
+    const curr_pos = { x: curr_e.clientX - slot_size.left, y: curr_e.clientY - slot_size.top}
+
+    let before_height = parseInt(window.getComputedStyle(time_slot, ':before').height) / 2;
+    let after_height = parseInt(window.getComputedStyle(time_slot, ':after').height) / 2;
+
+    let height_per_min = slot_size.height / time_slots[time_slot.dataset.slot].length;
+
+    let start_action;
+
+    if (start_pos.y < before_height) {
+        start_action = 'top';
+    } else if (start_pos.y > slot_size.height - after_height) {
+        start_action = 'bottom';
+    } else {
+        start_action = 'middle';
+    }
+
+    let length = time_slots[time_slot.dataset.slot].length;
+    let hour = time_slots[time_slot.dataset.slot].time.getHours();
+    let minute = time_slots[time_slot.dataset.slot].time.getMinutes();
+    let min_quarter = Math.floor(minute / 15);
+
+    console.log(start_action)
+
+    if (curr_e.movementY < 0) {
+        switch (start_action) {
+            case 'top':
+                time_slots[time_slot.dataset.slot].time.setMinutes(time_slots[time_slot.dataset.slot].time.getMinutes() - 15);
+                time_slot.style.top = `${parseInt(time_slot.style.top) - (height_per_min * 15)}px`;
+                time_slots[time_slot.dataset.slot].length += 15;
+                time_slot.style.height = `${height_per_min * (time_slots[time_slot.dataset.slot].length)}px`;
+                break;
+            case 'bottom':
+                time_slots[time_slot.dataset.slot].length -= 15;
+                time_slot.style.height = `${height_per_min * (time_slots[time_slot.dataset.slot].length)}px`;
+                break;
+            case 'middle':
+                break;
+        }
+    } else if (curr_e.movementY > 0) {
+        switch (start_action) {
+            case 'top':
+                time_slots[time_slot.dataset.slot].time.setMinutes(time_slots[time_slot.dataset.slot].time.getMinutes() + 15);
+                time_slot.style.top = `${parseInt(time_slot.style.top) + (height_per_min * 15)}px`;
+                time_slots[time_slot.dataset.slot].length -= 15;
+                time_slot.style.height = `${height_per_min * (time_slots[time_slot.dataset.slot].length)}px`;
+                break;
+            case 'bottom':
+                time_slots[time_slot.dataset.slot].length += 15;
+                time_slot.style.height = `${height_per_min * (time_slots[time_slot.dataset.slot].length)}px`;
+                break;
+            case 'middle':
+                break;
+        }
+    }
+
+    time_slot.title = `${(hour % 12) || 12}:${minute < 10 ? '0' : ''}${minute} ${hour < 12 ? 'AM' : 'PM'} - ${length} minutes`;
+
+    time_slot.innerText = `${(hour % 12) || 12}:${minute < 10 ? '0' : ''}${minute} ${hour < 12 ? 'AM' : 'PM'}`;
+
+    time_slot.classList.remove('min15');
+    time_slot.classList.remove('min30');
+
+    if (length <= 15) {
+        time_slot.classList.add('min15');
+    } else if (length <= 30) {
+        time_slot.classList.add('min30');
+    } else if (length >= 60) {
+        time_slot.innerText += `\n${length / 60} hour${length != 60 ? 's' : ''}`;
+    }
+
+    // console.log(x, y, y - slot_size.height);
+}
+
 function create_time(time, length, td) {
 
     if (td === undefined) {
@@ -87,12 +167,36 @@ function create_time(time, length, td) {
     time_slot.style.left = `${td_size.left - wrap_size.left}px`;
     time_slot.style.top = `${(td_size.top - wrap_size.top) + min_quarter * (td_size.height / 4)}px`;
     time_slot.style.width = `${td_size.width}px`;
-    time_slot.style.height = `${td_size.height}px`;
+    time_slot.style.height = `${td_size.height / (60 / length)}px`;
+
+    time_slot.title = `${(hour % 12) || 12}:${minute < 10 ? '0' : ''}${minute} ${hour < 12 ? 'AM' : 'PM'} - ${length} minutes`;
 
     time_slot.innerText = `${(hour % 12) || 12}:${minute < 10 ? '0' : ''}${minute} ${hour < 12 ? 'AM' : 'PM'}`;
+
+    if (length <= 15) {
+        time_slot.classList.add('min15');
+    } else if (length <= 30) {
+        time_slot.classList.add('min30');
+    } else if (length >= 60) {
+        time_slot.innerText += `\n${length / 60} hour${length != 60 ? 's' : ''}`;
+    }
+
+    // time_slot.draggable = true;
+    time_slot.addEventListener('mousedown', (e) => {
+        edit_event = (new_e) => {
+            edit_time(e, new_e);
+        };
+        document.body.addEventListener('mousemove', edit_event);
+        document.body.addEventListener('mouseup', () => {
+            document.body.removeEventListener('mousemove', edit_event);
+        });
+    });
+
+    time_slot.dataset.slot = time_slots.length;
+
     t_wrap.appendChild(time_slot);
 
-    time_slots.push({ time_slot: time, length: length, elem: time_slot });
+    time_slots.push({ time: time, length: length, elem: time_slot });
 }
 
 function create_click(e) {
